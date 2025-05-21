6a7dc0c904149e2378cb87608ddf6ab25e0b575b
Lorenzo Milamonti <milamontilorenzo@gmail.com>
2025-05-20 22:24:18 -0300

    Add files via upload

---
+++ hostexpress_site/js/login.js
@@ -0,0 +1,37 @@
+async function Login() {
+    if (!$("#email").val() || !$("#senha").val()){
+        alert('preencha todos os campos!');
+    }
+    let formData = new FormData();
+    formData.append("EMAIL", $("#email").val());
+    formData.append("SENHA", $("#senha").val());
+
+    let response = await fetch(`../php/login.php`, {
+        body: formData,
+        method: "POST"
+    });
+    let data = await response.json();
+
+    if(data.status == 'true'){
+        window.location.href = "/index.html";
+    }
+    else{
+        alert('E-mail ou senha incorretos!');
+    }
+}
+
+//precisa arruma essa merda ainda 
+$(document).ready(function () {
+    $('#ver_senha').click(function () {
+        const input = $('#senha');
+        const icone = $('#icone');
+
+        if (input.attr('type') === 'password') {
+          input.attr('type', 'text');
+          icone.removeClass('bi-eye').addClass('bi-eye-slash');
+        } else {
+          input.attr('type', 'password');
+          icone.removeClass('bi-eye-slash').addClass('bi-eye');
+        }
+    });
+});
