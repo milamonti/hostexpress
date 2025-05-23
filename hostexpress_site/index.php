75625fdebbcf5f3630874cf3ade2189c15312838
Lorenzo Milamonti <milamontilorenzo@gmail.com>
2025-05-22 21:30:10 -0300

    adicionar script do arquivo js no index

--- hostexpress_site/index.php
+++ hostexpress_site/index.php
@@ -263,6 +263,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <script src="./js/index.js"></script>
+    
 </body>
 </html>
+
